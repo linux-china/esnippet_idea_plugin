f6fa8c66c3b2526f81e0476f84955f44f2b79c88
libing.chen <libing.chen@907f3344-9b2b-11dd-bde8-492b52a95b86>
2008-10-16 03:33:13 +0000

    esnippet source code
    git-svn-id: http://esnippet.googlecode.com/svn/trunk/snippet_plugin@27 907f3344-9b2b-11dd-bde8-492b52a95b86

---
+++ src/org/mvnsearch/snippet/impl/jdaa/JDAASnippetSearchAgent.java
@@ -0,0 +1,172 @@
+package org.mvnsearch.snippet.impl.jdaa;
+
+import org.mvnsearch.snippet.Category;
+import org.mvnsearch.snippet.Comment;
+import org.mvnsearch.snippet.Snippet;
+import org.mvnsearch.snippet.SnippetSearchAgent;
+
+import java.util.*;
+
+/**
+ * Java developer Almanac 1.4 snippet search agent
+ *
+ * @author Anki R Nelaturu
+ */
+public class JDAASnippetSearchAgent extends SnippetSearchAgent {
+    private List<JDAASnippet> snippetList = new ArrayList<JDAASnippet>(); //snippet list
+    private Map<String, Category> categoryMap = new HashMap<String, Category>(); //category map
+
+    /**
+     * java Almanac search agent
+     *
+     * @throws Exception exception
+     */
+    public JDAASnippetSearchAgent() throws Exception {
+        Properties p = new Properties();
+        p.load(JDAASnippetSearchAgent.class.getResourceAsStream("/org/mvnsearch/snippet/impl/jdaa/snippets.properties"));
+        int count = Integer.parseInt(p.getProperty("snippets.count"));
+        for (int i = 0; i < count; i++) {
+            String href = p.getProperty("snippet.href." + i);
+            JDAASnippet jdaSnippet = new JDAASnippet(i, href, p.getProperty("snippet.description." + i));
+            snippetList.add(jdaSnippet);
+            //fill category
+            if (href.contains("/")) {
+                String javaPackage = href.substring(0, href.indexOf("/"));
+                if (!categoryMap.containsKey(javaPackage)) {
+                    categoryMap.put(javaPackage, new Category(javaPackage, javaPackage));
+                }
+            }
+        }
+    }
+
+    /**
+     * get agent description
+     *
+     * @return description
+     */
+    public String getDescription() {
+        return "This website supplements the book The Java Developers Almanac 1.4. All the code examples from the book are made available here for you to copy and paste into your programs.";
+    }
+
+    /**
+     * get home page url
+     *
+     * @return home page url
+     */
+    public String getHomePageURL() {
+        return "http://javaalmanac.com/";
+    }
+
+    /**
+     * get display name
+     *
+     * @return display name
+     */
+    public String getDisplayName() {
+        return "The Java Developers Almanac 1.4";
+    }
+
+    /**
+     * get agent id
+     *
+     * @return agent id
+     */
+    public String getId() {
+        return "Almanac";
+    }
+
+    /**
+     * Performs the search for given keywords
+     *
+     * @param keywords - Array of keywords
+     * @return Matching araaylist of the ISnippets
+     */
+    public List<Snippet> query(String[] keywords) {
+        List<Snippet> matchingList = new ArrayList<Snippet>();
+        for (JDAASnippet url : snippetList) {
+            if (url.matches(keywords)) {
+                matchingList.add(url);
+            }
+        }
+        return matchingList;
+    }
+
+    /**
+     * find snippets according to snippets
+     *
+     * @param categoryId category id
+     * @return snippet list
+     */
+    public List<Snippet> findSnippetsByCategory(String categoryId) {
+        return query(new String[]{categoryId + "/"});
+    }
+
+    /**
+     * find recent added snippets for RSS
+     *
+     * @return snippets
+     */
+    public List<Snippet> findRecentAddedSnippets() {
+        return null;
+    }
+
+    /**
+     * find snippet by id
+     *
+     * @param snippetId snippet id
+     * @return snippet object
+     */
+    public Snippet findById(String snippetId) {
+        return null;
+    }
+
+    /**
+     * update snippet category
+     *
+     * @param snippet snippet object
+     * @return result
+     */
+    public boolean updateSnippet(Snippet snippet) {
+        return false;
+    }
+
+    /**
+     * find snippet comment
+     *
+     * @param snippetId snippet id
+     * @return comment list
+     */
+    public List<Comment> findComments(String snippetId) {
+        return null;
+    }
+
+    /**
+     * add snippet comment
+     *
+     * @param snippetId snippet id
+     * @param comment   comment
+     * @return result
+     */
+    public boolean addSnippetComment(String snippetId, Comment comment) {
+        return false;
+    }
+
+    /**
+     * get category list
+     *
+     * @return category list
+     */
+    public Collection<Category> findRootCategories() {
+        return categoryMap.values();
+    }
+
+    /**
+     * get code content type
+     *
+     * @return code content type
+     */
+    public String getCodeContentType() {
+        return "text/java";
+    }
+
+}
