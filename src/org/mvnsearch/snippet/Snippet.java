15a636275ff581e0d01f149a3b85fc038c84c111
libing.chen <libing.chen@907f3344-9b2b-11dd-bde8-492b52a95b86>
2008-10-16 03:59:16 +0000

    esnippet source code
    git-svn-id: http://esnippet.googlecode.com/svn/trunk/snippet_plugin@79 907f3344-9b2b-11dd-bde8-492b52a95b86

---
+++ src/org/mvnsearch/snippet/Snippet.java
@@ -0,0 +1,72 @@
+package org.mvnsearch.snippet;
+
+/**
+ * snippet object
+ *
+ * @author Anki R Nelaturu
+ */
+public class Snippet {
+    private String id;   //id
+    private String title; //title
+    private String code;  //code
+    private String description; //description
+    private String detailUrl; //œÍ«Èurl
+    private String author;    //author
+    private String language; //language type
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDetailUrl() {
+        return detailUrl;
+    }
+
+    public void setDetailUrl(String detailUrl) {
+        this.detailUrl = detailUrl;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+}
