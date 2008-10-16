e42d597fd6b41ec1fc12c7f352dfcc704b21321d
libing.chen <libing.chen@907f3344-9b2b-11dd-bde8-492b52a95b86>
2008-10-16 03:56:43 +0000

    esnippet source code
    git-svn-id: http://esnippet.googlecode.com/svn/trunk/snippet_plugin@74 907f3344-9b2b-11dd-bde8-492b52a95b86

---
+++ src/org/mvnsearch/snippet/plugin/ui/tree/RootNode.java
@@ -0,0 +1,17 @@
+package org.mvnsearch.snippet.plugin.ui.tree;
+
+import javax.swing.tree.DefaultMutableTreeNode;
+
+/**
+ * root node for tree
+ *
+ * @author Anki R Nelaturu
+ */
+public class RootNode extends DefaultMutableTreeNode {
+    /**
+     * construct root node
+     */
+    public RootNode() {
+        super("Snippet Pro");
+    }
+}
