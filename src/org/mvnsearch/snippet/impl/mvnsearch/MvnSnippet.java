bcc614470645a7dd99d5d7b97a3ae49b0f478278
libing.chen <libing.chen@907f3344-9b2b-11dd-bde8-492b52a95b86>
2008-10-16 03:34:42 +0000

    esnippet source code
    git-svn-id: http://esnippet.googlecode.com/svn/trunk/snippet_plugin@30 907f3344-9b2b-11dd-bde8-492b52a95b86

---
+++ src/org/mvnsearch/snippet/impl/mvnsearch/MvnSnippet.java
@@ -0,0 +1,82 @@
+package org.mvnsearch.snippet.impl.mvnsearch;
+
+import org.mvnsearch.snippet.Snippet;
+
+/**
+ * mvn snippet
+ *
+ * @author linux_china@hotmail.com
+ */
+public class MvnSnippet extends Snippet {
+    private String id;   //id
+    private String categoryId;  //category id
+    private String mnemonic;    //mnemonic
+    private String keywords;   //keywords
+    private String url;    //url
+    private String rawDescription;  //raw description
+
+    public String getId() {
+        return String.valueOf(id);
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(String categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getMnemonic() {
+        return mnemonic;
+    }
+
+    public void setMnemonic(String mnemonic) {
+        this.mnemonic = mnemonic;
+    }
+
+    public String getKeywords() {
+        return keywords;
+    }
+
+    public void setKeywords(String keywords) {
+        this.keywords = keywords;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String toString() {
+        return getTitle();
+    }
+
+    @Override
+    public String getDescription() {
+        String descriptionHeader = "<ul>Basic Information:\n" +
+                "    <li>ID: " + getId() + "</li>\n" +
+                "    <li>Name: " + getTitle() + "</li>\n" +
+                "    <li>Mnemonic:" + getMnemonic() + "</li>\n" +
+                "    <li>Author: " + getAuthor() + "</li>\n" +
+                "    <li>Keywords:" + getKeywords() + "</li>\n" +
+                "</ul><br/><br/>";
+        if (getRawDescription() == null) return descriptionHeader;
+        else return descriptionHeader + getRawDescription();
+    }
+
+    public String getRawDescription() {
+        return rawDescription;
+    }
+
+    public void setRawDescription(String rawDescription) {
+        this.rawDescription = rawDescription;
+    }
+}
