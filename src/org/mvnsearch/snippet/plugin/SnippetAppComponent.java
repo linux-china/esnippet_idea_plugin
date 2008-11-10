82676185558dad61c398547e78c1d2b45d2da06a
libing.chen <libing.chen@907f3344-9b2b-11dd-bde8-492b52a95b86>
2008-11-10 06:07:07 +0000

    minor update
    git-svn-id: http://esnippet.googlecode.com/svn/trunk/snippet_plugin@399 907f3344-9b2b-11dd-bde8-492b52a95b86

--- src/org/mvnsearch/snippet/plugin/SnippetAppComponent.java
+++ src/org/mvnsearch/snippet/plugin/SnippetAppComponent.java
@@ -13,6 +13,12 @@ import org.mvnsearch.snippet.plugin.ui.RepositoryInfoForm;
 import org.mvnsearch.snippet.impl.mvnsearch.SnippetService;
 
 import javax.swing.*;
+import javax.swing.text.EditorKit;
+
+import jsyntaxpane.syntaxkits.*;
+
+import java.util.Map;
+import java.util.HashMap;
 
 /**
  * snippet application component
@@ -24,6 +30,7 @@ public class SnippetAppComponent implements ApplicationComponent {
     public String serviceUrl;  //service url
     public String userName;    //user name
     private RepositoryInfoForm form = new RepositoryInfoForm();
+    private Map<String, EditorKit> editorKits = new HashMap<String, EditorKit>();
 
     /**
      * get snippet project component
@@ -32,6 +39,16 @@ public class SnippetAppComponent implements ApplicationComponent {
      */
     public static SnippetAppComponent getInstance() {
         return ApplicationManager.getApplication().getComponent(SnippetAppComponent.class);
+    }
+
+    /**
+     * get editor kit according to content type
+     *
+     * @param contentType content type
+     * @return editor kit
+     */
+    public EditorKit getEditorKit(String contentType) {
+        return editorKits.get(contentType.toLowerCase());
     }
 
     /**
@@ -47,7 +64,20 @@ public class SnippetAppComponent implements ApplicationComponent {
     }
 
 
+    /**
+     * init component: editorkit regestration, init snippet service
+     */
     public void initComponent() {
+        jsyntaxpane.DefaultSyntaxKit.initKit();
+        editorKits.put("text/c", new CSyntaxKit());
+        editorKits.put("text/cpp", new CppSyntaxKit());
+        editorKits.put("text/java", new JavaSyntaxKit());
+        editorKits.put("text/groovy", new GroovySyntaxKit());
+        editorKits.put("text/javascript", new JavaScriptSyntaxKit());
+        editorKits.put("text/sql", new SqlSyntaxKit());
+        editorKits.put("text/xml", new XmlSyntaxKit());
+        editorKits.put("text/python", new PythonSyntaxKit());
+        editorKits.put("text/ruby", new RubySyntaxKit());
         initSnippetService();
     }
 
