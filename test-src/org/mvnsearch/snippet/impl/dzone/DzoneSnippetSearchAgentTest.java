3448c87e5a547e1f83cd5dd6d1b106bdaac0e1b9
libing.chen <libing.chen@907f3344-9b2b-11dd-bde8-492b52a95b86>
2009-01-23 07:24:49 +0000

    minor update
    git-svn-id: http://esnippet.googlecode.com/svn/trunk/snippet_plugin@438 907f3344-9b2b-11dd-bde8-492b52a95b86

--- test-src/org/mvnsearch/snippet/impl/dzone/DzoneSnippetSearchAgentTest.java
+++ test-src/org/mvnsearch/snippet/impl/dzone/DzoneSnippetSearchAgentTest.java
@@ -1,6 +1,9 @@
 package org.mvnsearch.snippet.impl.dzone;
 
 import junit.framework.TestCase;
+import org.mvnsearch.snippet.Snippet;
+
+import java.util.List;
 
 /**
  * test case for dzone agent
@@ -11,7 +14,10 @@ public class DzoneSnippetSearchAgentTest extends TestCase {
     DzoneSnippetSearchAgent agent = new DzoneSnippetSearchAgent();
 
     public void testQuery() {
-        String[] keywords = new String[]{"java"};
-        agent.query(keywords);
+        String[] keywords = new String[]{"mail"};
+        List<Snippet> snippets = agent.query(keywords);
+        for (Snippet snippet : snippets) {
+            System.out.println(snippet.getTitle());
+        }
     }
 }
